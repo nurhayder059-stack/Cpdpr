6f203c2fbf2de0783f27dd79980e8dd4f0f484f7
Hayder <nurhayder059@gmail.com>
2026-06-01 18:20:13 +0300

    Update equalshot.py

--- equalshot.py
+++ equalshot.py
@@ -3,7 +3,7 @@ n, m = map(int, input().split())
 alcohol1 = 0
 volume1 = 0
 
-for _ in range(n):
+for i in range(n):
     a, p = map(int, input().split())
     volume1 += a
     alcohol1 += a * p
@@ -11,7 +11,7 @@ for _ in range(n):
 alcohol2 = 0
 volume2 = 0
 
-for _ in range(m):
+for i in range(m):
     a, p = map(int, input().split())
     volume2 += a
     alcohol2 += a * p
